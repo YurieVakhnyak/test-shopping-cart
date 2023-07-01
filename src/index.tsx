b2b9d9ce649c3846183b7c66dc56ed834c0a5257
YVakhnyak <namejfk@gmail.com>
2023-07-01 06:00:22 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/test-shopping-cart/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
